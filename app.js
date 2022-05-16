20bf8e159519687418d814b3a2ba1fddc5ce2eb3
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2022-05-16 20:01:35 +0200

    Simple startup of templating using EJS, ejs files should be in views folder

--- app.js
+++ app.js
@@ -2,9 +2,32 @@ const express = require("express");
 const bodyParser = require("body-parser");
  const app = express();
 
+// Telling our app to use ejs
+// Documentation => gitHub, EJS with expresJs
+app.set('view engine', 'ejs');
+
+
  app.get("/", (req, res)=>{
-   res.send("Hello");
+
+
+   // storing the day of week in a variable
+   const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+   const d = new Date();
+   let day = weekday[d.getDay()];
+
+
+
+
+   // Express is going to look inside a folder called views and it's going to look
+   // for a file that's called list and it has the extension of ejs.
+   // passing single variable and value in an object
+   res.render("list", {kindOfDay: day});
+
+
+
  });
+
+
 
  app.listen(3000, ()=>{
    console.log("Server started on port 3000")
