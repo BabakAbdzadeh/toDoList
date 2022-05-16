8b4667eb3fef16d72c9976157d4ca0dab6724e14
Babak Abdolah Zadeh <babak.abdzadeh@gmail.com>
2022-05-16 21:48:45 +0200

    Passing Data from WebPage to Server and passing back after doing stuff on it

--- app.js
+++ app.js
@@ -1,34 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
- const app = express();
+const app = express();
+
+
 
 // Telling our app to use ejs
 // Documentation => gitHub, EJS with expresJs
 app.set('view engine', 'ejs');
 
-
- app.get("/", (req, res)=>{
-
-
-   // storing the day of week in a variable
-   const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-   const d = new Date();
-   let day = weekday[d.getDay()];
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
+// Global variables:
+let itemsarray = [];
+
+app.get("/", (req, res) => {
 
 
-   // Express is going to look inside a folder called views and it's going to look
-   // for a file that's called list and it has the extension of ejs.
-   // passing single variable and value as an object
-   res.render("list", {kindOfDay: day});
+  // storing the day of week in a variable
+  var options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+  };
+  var today = new Date();
+  var day = today.toLocaleDateString("en-US", options); // E.G. Saturday, September 17, 2016
 
 
 
- });
+  // Express is going to look inside a folder called views and it's going to look
+  // for a file that's called list and it has the extension of ejs.
+  // passing single variable and value as an object
+  res.render("list", {
+    kindOfDay: day,
+    items: itemsarray,
+  });
+});
 
 
+// gets data from form, and pass after doing logic stuff redirect it
+app.post('/', (req, res) => {
+  let item = req.body.toDo;
+  itemsarray.push(item);
 
- app.listen(3000, ()=>{
-   console.log("Server started on port 3000")
- });
+  // Important to redirect, otherwise it waits on pending!
+  res.redirect("/");
+});
+
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000")
+});
